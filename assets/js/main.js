3ae3fec23ec376cefcf42c89ffc8cee0c7fbc174
darchard1984 <32482170+darchard1984@users.noreply.github.com>
2018-09-13 16:00:28 +0100

    reverted js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,22 +2,21 @@
 
     // ADD/REMOVE CLICKED CLASS TO BURGER BARS ON CLICK //
 
-    const dropdownContainer = document.getElementById('dropdown');
-    //const burgerMenu = document.getElementById('burger-menu'); 
+    const burgerMenu = document.getElementById('burger-menu'); 
     const barOne = document.getElementById('bar-1');
     const barTwo = document.getElementById('bar-2');
     const barThree = document.getElementById('bar-3');
 
-    dropdownContainer.addEventListener('click', () => {
+    burgerMenu.addEventListener('click', () => {
         
-        if (dropdownContainer.classList.contains('open')) {
-            console.log('open');
+        if (barOne.classList.contains('cross-container--clicked-cross-bar-1')) {
+
             barOne.classList.remove('cross-container--clicked-cross-bar-1');
             barTwo.classList.remove('cross-container--clicked-cross-bar-2');
             barThree.classList.remove('cross-container--clicked-cross-bar-3');
 
-        } else if (dropdownContainer.classList.contains('open') == false) {
-            console.log('close');
+        } else {
+
             barOne.classList.add('cross-container--clicked-cross-bar-1');
             barTwo.classList.add('cross-container--clicked-cross-bar-2');
             barThree.classList.add('cross-container--clicked-cross-bar-3');
