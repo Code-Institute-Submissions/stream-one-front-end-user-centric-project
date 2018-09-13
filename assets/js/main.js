3ee9fe92fb17d0c52c463b1f3a67534f41b9629c
darchard1984 <32482170+darchard1984@users.noreply.github.com>
2018-09-13 16:47:14 +0100

    removed hover style on click

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,14 +2,14 @@
 
     // ADD/REMOVE CLICKED CLASS TO BURGER BARS ON CLICK //
 
-    const burgerMenu = document.getElementById('dropdown-wrapper'); 
+    const burgerMenu = document.getElementById('dropdown'); 
     const barOne = document.getElementById('bar-1');
     const barTwo = document.getElementById('bar-2');
     const barThree = document.getElementById('bar-3');
 
     burgerMenu.addEventListener('click', () => {
         
-        if (barOne.classList.contains('cross-container--clicked-cross-bar-1')) {
+        if (burgerMenu.classList.contains('open')) {
 
             barOne.classList.remove('cross-container--clicked-cross-bar-1');
             barTwo.classList.remove('cross-container--clicked-cross-bar-2');
