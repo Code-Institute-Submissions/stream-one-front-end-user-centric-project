de44f47dc11ce550c840c89824cef9fa7155f3ee
darchard1984 <32482170+darchard1984@users.noreply.github.com>
2018-09-13 14:55:41 +0100

    removed focus on all burger bars

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,7 @@
     const burgerMenu = document.getElementById('burger-menu'); 
     const barOne = document.getElementById('bar-1');
     const barTwo = document.getElementById('bar-2');
+    const barThree = document.getElementById('bar-3');
 
     burgerMenu.addEventListener('click', () => {
         
@@ -12,11 +13,13 @@
 
             barOne.classList.remove('cross-container--clicked-cross-bar-1');
             barTwo.classList.remove('cross-container--clicked-cross-bar-2');
+            barThree.classList.remove('cross-container--clicked-cross-bar-3');
 
         } else {
 
             barOne.classList.add('cross-container--clicked-cross-bar-1');
             barTwo.classList.add('cross-container--clicked-cross-bar-2');
+            barThree.classList.add('cross-container--clicked-cross-bar-3');
 
         }
        
