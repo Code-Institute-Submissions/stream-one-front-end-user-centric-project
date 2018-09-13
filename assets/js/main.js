96c0fe39163cb929d67a212b14e70c351a4cdb7c
darchard1984 <32482170+darchard1984@users.noreply.github.com>
2018-09-13 18:07:44 +0100

    refactored back to ES6

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 (() => {
 
-    // ADD/REMOVE CLICKED CLASS TO BURGER BARS ON CLICK //
+    // ADD/REMOVE CLICKED STYLES TO BURGER BARS ON MENU ICON CLICK //
 
     const burgerMenu = document.getElementById('dropdown'); 
     const barOne = document.getElementById('bar-1');
@@ -8,19 +8,16 @@
     const barThree = document.getElementById('bar-3');
 
 
-    burgerMenu.addEventListener('click', function() {
-        console.log('click');
+    burgerMenu.addEventListener('click', () => {
+       
         if (burgerMenu.classList.contains('open') == true) {
-            console.log(true);
+           
             barOne.classList.remove('cross-container--clicked-cross-bar-1');
             barTwo.classList.remove('cross-container--clicked-cross-bar-2');
             barThree.classList.remove('cross-container--clicked-cross-bar-3');
 
-        }
-        
-        if (burgerMenu.classList.contains('open') == false) {
+        } else if (burgerMenu.classList.contains('open') == false) {
 
-            console.log(false);
             barOne.classList.add('cross-container--clicked-cross-bar-1');
             barTwo.classList.add('cross-container--clicked-cross-bar-2');
             barThree.classList.add('cross-container--clicked-cross-bar-3');
